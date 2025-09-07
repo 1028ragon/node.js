08300f3cf9e4742e213a0cf4372c2de27fea83a7
1028ragon <1028ragon@naver.com>
2025-09-07 14:58:48 +0900

    ~7강 MongoDB와 서버 연결

--- server.js
+++ server.js
@@ -1,18 +1,30 @@
 const express = require('express')
 const app = express()
 
-app.listen(8080, () => {
+app.use(express.static(__dirname + '/public'))
+
+const { MongoClient } = require('mongodb');
+
+let db
+const url = 'mongodb+srv://1028ragon:npcs090909@1028ragon.oaooppv.mongodb.net/?retryWrites=true&w=majority&appName=1028ragon'
+new MongoClient(url).connect().then((client)=>{
+  console.log('DB연결성공')
+  db = client.db('forum')
+  app.listen(8080, () => {
     console.log('http://localhost:8080 에서 서버 실행중')
 })
+}).catch((err)=>{
+  console.log(err)
+})
+
 
 app.get('/', (요청, 응답) => {
     응답.sendFile(__dirname + '/index.html')
 }) // 유저에게 html 파일 보내주려면
 
-app.get('/shop', (요청, 응답) => {
-    응답.send('쇼핑페이지입니다')
+app.get('/news', (요청, 응답) => {
+    db.collection('post').insertOne({title : '어쩌구'})
+    // 응답.send('오늘 비옴')
 })
 
-app.get('/about', (요청, 응답) => {
-    응답.sendFile(__dirname + '/about.html')
-})
+
